9d7a634d03d9e02ad373e4ad80f8605dbdbdde24
Michelle Yun <yunmichelle@meta.com>
2023-11-20 23:51:34 -0800

    stylesheet

--- glossary/urls.py
+++ glossary/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('metric/<int:primary_key>', views.metric_detail_view, name='metric-detail'),
     path('metrics/', views.metric_intro_view, name='metric-intro')
+]
+# Add Django site authentication urls (for login, logout, password management)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
